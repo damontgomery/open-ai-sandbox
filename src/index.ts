8cfb65914c425a96af59e4899060186456816ef9
Dan Montgomery <dmontgomery@phase2technology.com>
2023-12-08 17:19:09 -0600

    refactored to put tools responses in the openAI library

--- src/index.ts
+++ src/index.ts
@@ -90,67 +90,18 @@ async function main() {
     }
   );
 
-  const submitToolsOutput = async (run: OpenAI.Beta.Threads.Run) => {
-    if (run.status !== 'requires_action' || run.required_action?.type !== 'submit_tool_outputs') {
-      return
-    }
-
-    const toolCalls = run.required_action.submit_tool_outputs.tool_calls
-
-    console.log('toolCalls', toolCalls)
-
-    const toolOutputs = toolCalls.map(toolCall => {
-      const {name: functionName, arguments: functionArgumentsRaw} = toolCall.function
-      const functionArguments = JSON.parse(functionArgumentsRaw)
-      
-      if (!availableToolFunctions.has(functionName)) {
-        return
-      }
-      
-      let functionResponse = (availableToolFunctions.get(functionName) as Function)(functionArguments)
-
-      if (typeof functionResponse !== 'string') {
-        functionResponse = JSON.stringify(functionResponse)
-      }
-
-      console.log('functionResponse', functionResponse)
-      return {
-        tool_call_id: toolCall.id,
-        output: functionResponse,
-      }
+  getRunResponse({
+    threadId: thread.id,
+    runId: run.id,
+    availableToolFunctions,
+    openai
+  })
+    .then(async () => {
+      logThreadMessages({threadId: thread.id, openai})
     })
-      .filter(toolOutput => toolOutput !== undefined) as OpenAI.Beta.Threads.Runs.RunSubmitToolOutputsParams.ToolOutput[]
-    
-    console.log('toolOutputs', toolOutputs)
-
-    const toolSubmitRun = await openai.beta.threads.runs.submitToolOutputs(
-      thread.id,
-      run.id,
-      {
-        tool_outputs: toolOutputs,
-      }
-    );
-
-    getResponse({threadId: thread.id, runId: toolSubmitRun.id, openai})
-    
-  }
-
-  const getResponse = ({threadId, runId, openai}: {threadId: string; runId: string; openai: OpenAI}) => {
-    getRunResponse({threadId, runId, openai})
-      .then(async run => {
-        // console.log('run', run)
-  
-        submitToolsOutput(run)
-
-        logThreadMessages({threadId: thread.id, openai})
-      })
-      .catch(error => {
-        console.error('error', error)
-      })
-  }
-
-  getResponse({threadId: thread.id, runId: run.id, openai})
-
+    .catch(error => {
+      console.error('index getRunResponse error', error)
+    })
 }
 
 main()
--- src/openAIAssistantAPI.ts
+++ src/openAIAssistantAPI.ts
@@ -9,19 +9,80 @@ export const logThreadMessages = (
     openai: OpenAI
   }) => openai.beta.threads.messages.list(threadId)
   .then(threadMessages => {
+    console.log('threadMessages')
     threadMessages.data.forEach(message => {
       console.log(message, message.content)
     })
   })
 
+const submitToolsOutput = async (
+  {
+    run,
+    availableToolFunctions,
+    openai,
+  }: {
+    run: OpenAI.Beta.Threads.Run;
+    availableToolFunctions: Map<string, Function>;
+    openai: OpenAI;
+  }): Promise<OpenAI.Beta.Threads.Runs.Run> => {
+  if (run.status !== 'requires_action' || run.required_action?.type !== 'submit_tool_outputs') {
+    return run
+  }
+
+  const toolCalls = run.required_action.submit_tool_outputs.tool_calls
+
+  console.log('toolCalls', toolCalls)
+
+  const toolOutputs = toolCalls.map(toolCall => {
+    const {name: functionName, arguments: functionArgumentsRaw} = toolCall.function
+    const functionArguments = JSON.parse(functionArgumentsRaw)
+    
+    if (!availableToolFunctions.has(functionName)) {
+      return
+    }
+    
+    let functionResponse = (availableToolFunctions.get(functionName) as Function)(functionArguments)
+
+    if (typeof functionResponse !== 'string') {
+      functionResponse = JSON.stringify(functionResponse)
+    }
+
+    console.log('functionResponse', functionResponse)
+    return {
+      tool_call_id: toolCall.id,
+      output: functionResponse,
+    }
+  })
+    .filter(toolOutput => toolOutput !== undefined) as OpenAI.Beta.Threads.Runs.RunSubmitToolOutputsParams.ToolOutput[]
+  
+  console.log('toolOutputs', toolOutputs)
+
+  const toolSubmitRun = await openai.beta.threads.runs.submitToolOutputs(
+    run.thread_id,
+    run.id,
+    {
+      tool_outputs: toolOutputs,
+    }
+  );
+
+  return getRunResponse({
+    threadId: run.thread_id,
+    runId: toolSubmitRun.id,
+    availableToolFunctions,
+    openai
+  })
+}
+
 export const getRunResponse = (
   {
     threadId,
     runId,
+    availableToolFunctions,
     openai
   }: {
     threadId: string;
     runId: string;
+    availableToolFunctions: Map<string, Function>;
     openai: OpenAI
   }): Promise<OpenAI.Beta.Threads.Runs.Run> => new Promise((resolve, reject) => {
     const runChecker = setInterval(
@@ -32,20 +93,29 @@ export const getRunResponse = (
               clearInterval(runChecker)
             }
 
-            if (['completed', 'requires_action'].includes(run.status)) {
+            if (run.status === 'completed') {
               resolve(run)
+            }
+
+            if (run.status === 'requires_action') {
+              resolve(submitToolsOutput({run, availableToolFunctions, openai})
+              )
             }
 
             if (
               [
                 'failed',
                 'cancelled',
-                'requires_action',
                 'expired'
               ].includes(run.status)
             ) {
               reject(run)
             }
+          })
+          .catch(error => {
+            clearInterval(runChecker)
+            console.log('getRunResponse error', error)
+            reject(error)
           })
       },
       1000

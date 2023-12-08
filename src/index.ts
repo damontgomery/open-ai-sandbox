1d73b3e7bef4ee838048a6edb032a1a7a9ddeb52
Dan Montgomery <dmontgomery@phase2technology.com>
2023-12-08 14:58:24 -0600

    break some functions into a library

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv'
 import OpenAI from 'openai'
+import { getAssistantResponse, logThreadMessages } from './openAIAssistantAPI'
 
 dotenv.config()
 
@@ -8,42 +9,6 @@ console.log('OPENAI_API_KEY:', process.env.OPENAI_API_KEY)
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 })
-
-const logThreadMessages = (threadId: string) => openai.beta.threads.messages.list(threadId)
-  .then(threadMessages => {
-    threadMessages.data.forEach(message => {
-      console.log(message, message.content)
-    })
-  })
-
-const getAssistantResponse = ({threadId, runId}: {threadId: string; runId: string; }): Promise<OpenAI.Beta.Threads.Runs.Run> => new Promise((resolve, reject) => {
-    const runChecker = setInterval(
-      () => {
-        openai.beta.threads.runs.retrieve(threadId, runId)
-          .then(run => {
-            if (!['queued', 'in_progress'].includes(run.status)) {
-              clearInterval(runChecker)
-            }
-
-            if (run.status === 'completed') {
-              resolve(run)
-            }
-
-            if (
-              [
-                'failed',
-                'cancelled',
-                'requires_action',
-                'expired'
-              ].includes(run.status)
-            ) {
-              reject(run)
-            }
-          })
-      },
-      1000
-    )
-  })
 
 async function main() {
   const assistant = await openai.beta.assistants.create({
@@ -69,10 +34,10 @@ async function main() {
     }
   );
 
-  getAssistantResponse({threadId: thread.id, runId: run.id})
+  getAssistantResponse({threadId: thread.id, runId: run.id, openai})
     .then(run => {
       console.log('run', run)
-      logThreadMessages(thread.id)
+      logThreadMessages({threadId: thread.id, openai})
     })
 }
 
---
+++ src/openAIAssistantAPI.ts
@@ -0,0 +1,53 @@
+import OpenAI from 'openai'
+
+export const logThreadMessages = (
+  {
+    threadId,
+    openai
+  }: {
+    threadId: string;
+    openai: OpenAI
+  }) => openai.beta.threads.messages.list(threadId)
+  .then(threadMessages => {
+    threadMessages.data.forEach(message => {
+      console.log(message, message.content)
+    })
+  })
+
+export const getAssistantResponse = (
+  {
+    threadId,
+    runId,
+    openai
+  }: {
+    threadId: string;
+    runId: string;
+    openai: OpenAI
+  }): Promise<OpenAI.Beta.Threads.Runs.Run> => new Promise((resolve, reject) => {
+    const runChecker = setInterval(
+      () => {
+        openai.beta.threads.runs.retrieve(threadId, runId)
+          .then(run => {
+            if (!['queued', 'in_progress'].includes(run.status)) {
+              clearInterval(runChecker)
+            }
+
+            if (run.status === 'completed') {
+              resolve(run)
+            }
+
+            if (
+              [
+                'failed',
+                'cancelled',
+                'requires_action',
+                'expired'
+              ].includes(run.status)
+            ) {
+              reject(run)
+            }
+          })
+      },
+      1000
+    )
+  })
